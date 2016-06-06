cc91574d510f56d1fac19e69b7fb8d137e4c9809
tjtaill36@gmail.com <tjtaill36@gmail.com>
2016-06-06 12:02:11 -0400

    Added Diameter support

--- src/com/broadsoft/xslog/XsEventsToPlant.java
+++ src/com/broadsoft/xslog/XsEventsToPlant.java
@@ -38,13 +38,14 @@ public class XsEventsToPlant {
     private final static Pattern EVENT_LINE = Pattern.compile(XS_EVENT_ACTION + XS_EVENT + XS_EVENT_END);
 
     private final static Pattern DIRECTION_LINE =
-            Pattern.compile("\\s*udp \\d+ Bytes (IN from|OUT to) (.+?):\\d{1,5}");
+            Pattern.compile("\\s*(?:udp|tcp) \\d+ Bytes (IN from|OUT to) (.+?:\\d{1,5})");
     private final static Pattern SIP_RESPONSE_LINE =
             Pattern.compile("SIP/2.0 (\\d{3}).*?");
     private static final String METHOD =
             "(INVITE|ACK|BYE|CANCEL|OPTIONS|REGISTER|PRACK|SUBSCRIBE|NOTIFY|PUBLISH|INFO|REFER|MESSAGE|UPDATE)";
     private final static Pattern SIP_REQUEST_LINE =
             Pattern.compile(METHOD + " (:?sip|tel):.*?");
+    private final static Pattern DIAMETER_MESSAGE_LINE = Pattern.compile("\\s*commandCode=\\d+\\s+\\(([A-Z]+)\\)");
 
 
 
@@ -60,7 +61,7 @@ public class XsEventsToPlant {
     }
 
     public static void main(String[] args) throws IOException {
-        Direction direction;
+        Direction direction = Direction.OUT;
         String destination = null;
         String origin = null;
         String sipEndPoint = null;
@@ -126,6 +127,20 @@ public class XsEventsToPlant {
             // sip response line
             matcher = SIP_RESPONSE_LINE.matcher(line);
             if ( matcher.matches() ) {
+                messages.add(createMessage(matcher.group(1), origin, destination, lineNumber));
+                continue;
+            }
+
+            matcher = DIAMETER_MESSAGE_LINE.matcher(line);
+            if ( matcher.matches() ) {
+                switch(direction) {
+                    case IN:
+                        destination = "XS";
+                        break;
+                    case OUT:
+                        origin = "XS";
+                        break;
+                }
                 messages.add(createMessage(matcher.group(1), origin, destination, lineNumber));
                 continue;
             }
